57bbeeb4afbebda31571e62785970ad02ad0e1f7
b21527539 <zekeriyayakiskan@hacettepe.edu.tr>
2019-05-26 21:17:46 +0300

    this is s push try.

--- src/main/java/com/svms/sepetle/SepetleApplication.java
+++ src/main/java/com/svms/sepetle/SepetleApplication.java
@@ -1,6 +1,6 @@
 package com.svms.sepetle;
 
-
+// MY NAME IS ZEKOVIC
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
